a8240ce0e0abd78d9a07c995945da96adfecf9a4
Barsha <barsharoka2626@gmail.com>
2023-05-03 16:22:46 -0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class  MyApp extends StatelessWidget {
+  const MyApp ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+      Icon(Icons.star, color: Colors.black,),
+        Icon(Icons.star, color: Colors.red[500],),
+    Icon(Icons.star, color: Colors.black,),
+    Icon(Icons.star, color: Colors.red[500],),
+    Icon(Icons.star, color: Colors.black,),
+    ],
+    );
+
+    final ratings = Container(
+      padding: EdgeInsets.all(20),
+      child: Row,
+    )
+    return MaterialApp(
+    title: 'Demo Page',
+  home: Scaffold(
+  appBar: AppBar(
+  title: Text('Layout Demo'),
+  ),
+  body:
+    Row(
+
+
+      ],
+
+
+
+    )
+
+
+      ),
+
+    ) ;
+
+
+  }
+}
